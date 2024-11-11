6c9ccd4a7d590060cfb6fc4a716c577f34153b18
Antonio Senra Filho <acsenrafilho@gmail.com>
2024-11-11 17:42:50 -0300

    ENH: Added image samples (jpg, png, tif and pdf)

---
+++ tests/sample_paths.py
@@ -0,0 +1,15 @@
+import os
+
+# Sample text without noise
+SAMPLE_TEXT_JPG = (
+    '.' + os.sep + 'tests' + os.sep + 'files' + os.sep + 'sample-text-en.jpg'
+)
+SAMPLE_TEXT_PNG = (
+    '.' + os.sep + 'tests' + os.sep + 'files' + os.sep + 'sample-text-en.png'
+)
+SAMPLE_TEXT_TIF = (
+    '.' + os.sep + 'tests' + os.sep + 'files' + os.sep + 'sample-text-en.tif'
+)
+SAMPLE_TEXT_PDF = (
+    '.' + os.sep + 'tests' + os.sep + 'files' + os.sep + 'sample-text-en.pdf'
+)
