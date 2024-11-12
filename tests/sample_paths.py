c801a6f100bfbc6f04291407064e9807df4223d3
Antonio Senra Filho <acsenrafilho@gmail.com>
2024-11-12 18:17:10 -0300

    ENH: Added more image samples shortucut in sample_paths.py module

--- tests/sample_paths.py
+++ tests/sample_paths.py
@@ -13,3 +13,90 @@ SAMPLE_TEXT_TIF = (
 SAMPLE_TEXT_PDF = (
     '.' + os.sep + 'tests' + os.sep + 'files' + os.sep + 'sample-text-en.pdf'
 )
+
+# Samples single word without noise
+SAMPLE_CUCARACHA_RGB_PNG = (
+    '.'
+    + os.sep
+    + 'tests'
+    + os.sep
+    + 'files'
+    + os.sep
+    + 'sample-cucaracha-rgb-white-bg.png'
+)
+SAMPLE_CUCARACHA_GRAY_TIF = (
+    '.'
+    + os.sep
+    + 'tests'
+    + os.sep
+    + 'files'
+    + os.sep
+    + 'sample-cucaracha-gray-white-bg.tif'
+)
+
+# Samples single word with noise
+SAMPLE_CUCARACHA_GRAY_GAUSS_PNG = (
+    '.'
+    + os.sep
+    + 'tests'
+    + os.sep
+    + 'files'
+    + os.sep
+    + 'sample-cucaracha-gray-white-bg-gauss-noise.png'
+)
+SAMPLE_CUCARACHA_GRAY_SALT_PEPPER_PNG = (
+    '.'
+    + os.sep
+    + 'tests'
+    + os.sep
+    + 'files'
+    + os.sep
+    + 'sample-cucaracha-gray-white-bg-salt-pepper.png'
+)
+SAMPLE_CUCARACHA_RGB_GAUSS_PNG = (
+    '.'
+    + os.sep
+    + 'tests'
+    + os.sep
+    + 'files'
+    + os.sep
+    + 'sample-cucaracha-gray-white-bg-gauss-noise.png'
+)
+
+# Samples single word rotated
+SAMPLE_CUCARACHA_RGB_ROT_PLUS10_PNG = (
+    '.'
+    + os.sep
+    + 'tests'
+    + os.sep
+    + 'files'
+    + os.sep
+    + 'sample-cucaracha-rgb-white-bg-rot-plus10d.png'
+)
+SAMPLE_CUCARACHA_RGB_ROT_MINUS10_PNG = (
+    '.'
+    + os.sep
+    + 'tests'
+    + os.sep
+    + 'files'
+    + os.sep
+    + 'sample-cucaracha-rgb-white-bg-rot-minus10d.png'
+)
+SAMPLE_CUCARACHA_GRAY_ROT_PLUS10_PNG = (
+    '.'
+    + os.sep
+    + 'tests'
+    + os.sep
+    + 'files'
+    + os.sep
+    + 'sample-cucaracha-gray-white-bg-rot-plus10d.png'
+)
+SAMPLE_CUCARACHA_GRAY_ROT_MINUS10_PNG = (
+    '.'
+    + os.sep
+    + 'tests'
+    + os.sep
+    + 'files'
+    + os.sep
+    + 'sample-cucaracha-gray-white-bg-rot-minus10d.png'
+)
