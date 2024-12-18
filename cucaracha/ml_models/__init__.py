228aae94988a2653bba2461125a522d2c353f11e
Antonio Senra Filho <acsenrafilho@gmail.com>
2024-12-18 09:26:02 -0300

    BUG: Fix cucaracha models preset by the official Kaggle repository

--- cucaracha/ml_models/__init__.py
+++ cucaracha/ml_models/__init__.py
@@ -8,8 +8,8 @@ VALID_MODALITIES = [
 CUCARACHA_MODELS = {
     'image_classification': {
         'doc_is_signed': {
-            'variation': 'acsenrafilho/cucaracha-imgclass-document-is-signed/tensorFlow2/cucaracha-imgclass-document_is_signed-v0.1.0',
-            'dataset': 'acsenrafilho/cucaracha-mod-imgclass-constains-signature',
+            'variation': 'cucaracha-project/cucaracha-imgclass-document-is-signed/tensorFlow2/cucaracha-imgclass-document_is_signed-v0.1.0',
+            'dataset': 'cucaracha-project/cucaracha-mod-imgclass-constains-signature',
         }
     }
 }
