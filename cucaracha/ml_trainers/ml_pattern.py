4230d6293dfcd2e542e3edd842bfaf5e96e7f5ed
Antonio Senra Filho <acsenrafilho@gmail.com>
2025-01-03 14:56:37 -0300

    ENH: New function in MLPattern template for checking the architecture pattern

--- cucaracha/ml_trainers/ml_pattern.py
+++ cucaracha/ml_trainers/ml_pattern.py
@@ -1,6 +1,7 @@
 import os
 from abc import ABC, abstractmethod
 
+from cucaracha.ml_models.model_architect import ModelArchitect
 from cucaracha.ml_trainers.utils import (
     _check_dataset_folder,
     _check_dataset_folder_permissions,
@@ -10,17 +11,41 @@ from cucaracha.ml_trainers.utils import (
 
 class MLPattern(ABC):
     def __init__(self, dataset_path: str):  # pragma: no cover
-        _check_paths([dataset_path])
+        # _check_paths([dataset_path])
         self.dataset_path = os.path.abspath(dataset_path)
         self.batch_size = 64
         self.epochs = 500
 
     @abstractmethod
     def load_dataset(self):   # pragma: no cover
-        _check_dataset_folder(self.dataset_path)
+        # _check_dataset_folder(self.dataset_path)
         _check_dataset_folder_permissions(self.dataset_path)
         pass
 
     @abstractmethod
     def train_model(self):   # pragma: no cover
         pass
+
+
+def check_architecture_pattern(kwargs: dict, model_type=str):
+    if kwargs.get('architecture') and not isinstance(
+        kwargs.get('architecture'), ModelArchitect
+    ):
+        raise ValueError(
+            'The provided architecture is not a valid ModelArchitect instance.'
+        )
+    if (
+        kwargs.get('architecture')
+        and kwargs.get('architecture').modality != model_type
+    ):
+        raise ValueError(
+            f'The provided modality is not valid for {model_type} task.'
+        )
+
+    if (
+        kwargs.get('architecture')
+        and kwargs['architecture'].modality != model_type
+    ):
+        raise ValueError(
+            f'The provided architecture is not an {model_type} Architect instance.'
+        )
