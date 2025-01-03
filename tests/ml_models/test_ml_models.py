d381e43540c1cc5ac7121a19d192d5c11252ab74
Antonio Senra Filho <acsenrafilho@gmail.com>
2025-01-03 17:07:02 -0300

    ENH: Tests for new ML models: AlexNet, ResNet50, GoogleLeNet, DenseNet121 and ModelSoup

--- tests/ml_models/test_ml_models.py
+++ tests/ml_models/test_ml_models.py
@@ -3,6 +3,14 @@ import os
 import pytest
 
 from cucaracha.ml_models import CUCARACHA_PRESETS
+from cucaracha.ml_models.image_classification import (
+    AlexNet,
+    DenseNet121,
+    GoogleLeNet,
+    ModelSoup,
+    ResNet50,
+    SmallXception,
+)
 from cucaracha.ml_models.model_download import (
     download_cucaracha_dataset,
     download_cucaracha_model,
@@ -47,3 +55,45 @@ def test_download_cucaracha_dataset_raise_error_wrong_url():
     wrong_url = 'wrong-url.com/dataset'
     with pytest.raises(ValueError):
         download_cucaracha_dataset(wrong_url)
+
+
+def test_image_classification_model_small_xception_build_success():
+    model = SmallXception(img_shape=(128, 128), num_classes=2)
+    model.get_model()
+
+    assert model is not None
+
+
+def test_image_classification_model_alex_net_build_success():
+    model = AlexNet(img_shape=(128, 128), num_classes=2)
+    model.get_model()
+
+    assert model is not None
+
+
+def test_image_classification_model_google_le_net_build_success():
+    model = GoogleLeNet(img_shape=(128, 128), num_classes=2)
+    model.get_model()
+
+    assert model is not None
+
+
+def test_image_classification_model_resnet_50_build_success():
+    model = ResNet50(img_shape=(128, 128), num_classes=2)
+    model.get_model()
+
+    assert model is not None
+
+
+def test_image_classification_model_densenet_121_build_success():
+    model = DenseNet121(img_shape=(128, 128), num_classes=2)
+    model.get_model()
+
+    assert model is not None
+
+
+def test_image_classification_model_soup_build_success():
+    model = ModelSoup(img_shape=(128, 128), num_classes=2)
+    model.get_model()
+
+    assert model is not None
