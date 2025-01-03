09aa3c965ca6432c06adf6f5d69547852f2a8678
Antonio Senra Filho <acsenrafilho@gmail.com>
2025-01-03 17:07:28 -0300

    DOC: Documentation for Unet Xception image segmentation model

--- cucaracha/ml_models/image_segmentation/unet_xception.py
+++ cucaracha/ml_models/image_segmentation/unet_xception.py
@@ -5,6 +5,28 @@ from cucaracha.ml_models.model_architect import ModelArchitect
 
 
 class UNetXception(ModelArchitect):
+    """
+    UNetXception is a deep learning model for image segmentation that combines the
+    architecture of U-Net with the Xception model's depthwise separable convolutions.
+
+    Reference:
+        François Chollet. "Xception: Deep Learning with Depthwise Separable Convolutions."
+        arXiv preprint arXiv:1610.02357 (2017).
+
+    **kwargs: Arbitrary keyword arguments. Expected keys are:
+
+        - img_shape (tuple): Shape of the input images (height, width).
+
+        - num_classes (int): Number of classes for the segmentation task.
+
+    Methods:
+        get_model():
+            Builds and returns the UNetXception model.
+
+        __str__():
+            Returns a string representation of the model, including a summary of the model architecture.
+    """
+
     def __init__(self, **kwargs):
         super().__init__(modality='image_segmentation', **kwargs)
         self.img_shape = kwargs.get('img_shape')
