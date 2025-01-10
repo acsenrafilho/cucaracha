ad3eb8f8807fc819b0c2ea31d8ab9e7dee221507
Antonio Senra Filho <acsenrafilho@gmail.com>
2025-01-10 11:44:30 -0300

    ENH: Add tests for image classification and segmentation models' string representations

--- tests/ml_models/test_ml_models.py
+++ tests/ml_models/test_ml_models.py
@@ -11,7 +11,8 @@ from cucaracha.ml_models.image_classification import (
     ResNet50,
     SmallXception,
 )
-from cucaracha.ml_models.model_download import (
+from cucaracha.ml_models.image_segmentation import UNetXception
+from cucaracha.ml_models.kaggle_helpers import (
     download_cucaracha_dataset,
     download_cucaracha_model,
 )
@@ -97,3 +98,45 @@ def test_image_classification_model_soup_build_success():
     model.get_model()
 
     assert model is not None
+
+
+def test_image_classification_model_resnet_50_str():
+    model = ResNet50(img_shape=(128, 128), num_classes=2)
+    model_str = str(model)
+
+    assert 'Model Architecture modality: image_classification' in model_str
+
+
+def test_image_classification_model_densenet_121_str():
+    model = DenseNet121(img_shape=(128, 128), num_classes=2)
+    model_str = str(model)
+
+    assert 'Model Architecture modality: image_classification' in model_str
+
+
+def test_image_classification_model_soup_str():
+    model = ModelSoup(img_shape=(128, 128), num_classes=2)
+    model_str = str(model)
+
+    assert 'Model Architecture modality: image_classification' in model_str
+
+
+def test_image_classification_model_alex_net_str():
+    model = AlexNet(img_shape=(128, 128), num_classes=2)
+    model_str = str(model)
+
+    assert 'Model Architecture modality: image_classification' in model_str
+
+
+def test_image_classification_model_small_xception_str():
+    model = SmallXception(img_shape=(128, 128), num_classes=2)
+    model_str = str(model)
+
+    assert 'Model Architecture modality: image_classification' in model_str
+
+
+def test_image_segmentation_model_unet_xception_str():
+    model = UNetXception(img_shape=(128, 128), num_classes=2)
+    model_str = str(model)
+
+    assert 'Model Architecture modality: image_segmentation' in model_str
